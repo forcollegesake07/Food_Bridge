a1b244bca8416acf6fdd71c856eb38886c2dcc2b
forcollegesake07 <forcollegesake07@gmail.com>
2025-12-27 17:02:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -61,7 +61,6 @@ console.log("BREVO KEY EXISTS:", !!process.env.BREVO_API_KEY);
 const transporter = nodemailer.createTransport({
     host: "smtp-relay.brevo.com",
     port: 587,
-   secure: true,
     auth: {
         // Render will read these from your Environment Variables
         user: process.env.SMTP_USER, 
